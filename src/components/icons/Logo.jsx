fb20e32ae1bb262b2de838f40e8f049d179c848c
aldoBangun <punk.aldo68@gmail.com>
2022-09-19 18:44:36 +0800

    Initial setup

---
+++ src/components/icons/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <img src={logo} alt="logo" />
+    )
+  }
+}
+
+export default Logo
---
+++ src/components/layout/AppLayout.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Layout, Menu } from 'antd'
+import { HomeOutlined, UserOutlined, PhoneOutlined } from '@ant-design/icons'
+import { Outlet, Link } from 'react-router-dom'
+import Logo from '../icons/Logo'
+
+const menus = [
+  {
+    id: 1,
+    label: 'Home',
+    icon: HomeOutlined,
+    href: '/'
+  },
+  {
+    id: 2,
+    label: 'About',
+    icon: UserOutlined,
+    href: '/about'
+  },
+  {
+    id: 3,
+    label: 'Contact',
+    icon: PhoneOutlined,
+    href: '/contact'
+  }
+]
+
+const sidebarMenu = menus.map((menu) => ({
+  key: menu.id,
+  icon: React.createElement(menu.icon),
+  label: (
+    <Link to={menu.href}>
+      {menu.label}
+    </Link>
+  )
+}))
+
+class AppLayout extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Layout.Header>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </Layout.Header>
+        <Layout>
+          <Layout.Sider>
+            <Menu
+              items={sidebarMenu}
+            />
+          </Layout.Sider>
+
+          <Layout>
+            <Layout.Content>
+              <Outlet />
+            </Layout.Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    )
+  }
+}
+
+export default AppLayout
