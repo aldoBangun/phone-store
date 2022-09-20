bf73ef833dbe7fe205f17c7b660c55741b7fc67a
aldoBangun <punk.aldo68@gmail.com>
2022-09-20 22:03:26 +0800

    Add Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {
+  Card,
+  Space,
+  Typography,
+  Divider
+} from 'antd'
+import { PhoneOutlined, MailOutlined } from '@ant-design/icons'
 
 class Contact extends React.Component {
   render() {
     return (
-      <h1>Contact</h1>
+      <Card className="contact">
+        <Space>
+          <PhoneOutlined />
+          <Typography.Text copyable>0857-9237-3008</Typography.Text>
+        </Space>
+        <Divider />
+        <Space>
+          <MailOutlined />
+          <Typography.Text copyable>alfredobangun14@gmail.com</Typography.Text>
+        </Space>
+      </Card>
     )
   }
 }
